f39316b18bfd5528cbe92664c2e93fce5d4e53bc
joao <joaogalleni1376@gmail.com>
2026-03-20 16:37:09 -0300

    exercicio naming conventions finalizado

--- exercicios1703/NamingConvetions1703/src/main/java/Main.java
+++ exercicios1703/NamingConvetions1703/src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(){
-        System.out.println(NamingConventions.isFollowingConvention("ABEl", NamingConventions.Convention.CONSTANT));
+        System.out.println(NamingConventions.fromVariableToConst("maxValueTotal"));
     }
 }
--- exercicios1703/NamingConvetions1703/src/main/java/NamingConventions.java
+++ exercicios1703/NamingConvetions1703/src/main/java/NamingConventions.java
@@ -7,19 +7,38 @@ public class NamingConventions {
     }
 
     public static boolean isFollowingConvention(String string, Convention convention) {
-        boolean isFollowingConvention = false;
-        char firstChar = string.charAt(0);
-        if (Character.isLetter(firstChar) && !string.contains(String.valueOf(' '))) {
+        boolean isFollowing = false;
+        if (isValidJavaIdentifier(string)) {
+            char firstChar = string.charAt(0);
+
             if (convention == Convention.VARIABLE || convention == Convention.METHOD) {
-                isFollowingConvention = checkMethodAndVariable(string, firstChar);
+                isFollowing = checkMethodAndVariable(string, firstChar);
             } else if (convention == Convention.CLASS) {
-                isFollowingConvention = checkClass(string, firstChar);
+                isFollowing = checkClass(string, firstChar);
             } else {
-                isFollowingConvention = isValidConstant(string);
+                isFollowing = isValidConstant(string);
             }
         }
-        return isFollowingConvention;
+        return isFollowing;
 
+    }
+
+    public static boolean isValidJavaIdentifier(String string){
+        if (string.isEmpty() || string.contains(String.valueOf(' ')))
+            return false;
+        char character = string.charAt(0);
+
+        if(Character.isLetter(character)  || character == '_' || character == '$') {
+            for (int i = 1; i < string.length(); i++) {
+                character = string.charAt(i);
+                if ((!(Character.isLetterOrDigit(character)) && character != '_' && character != '$')) {
+                    return false;
+                }
+            }
+        }
+        else
+            return false;
+        return true;
     }
 
     private static boolean checkMethodAndVariable(String string, char firstChar) {
@@ -41,13 +60,14 @@ public class NamingConventions {
 
         for (int i = 1; i < string.length(); i++) {
             character = string.charAt(i);
-            if (!Character.isLetter(character) && !Character.isDigit(character) || upperCaseCounter == 2)
+            if (!Character.isLetter(character) || upperCaseCounter == 2)
                 break;
-            if (Character.isUpperCase(character))
+            if (Character.isUpperCase(character)) {
                 upperCaseCounter++;
+            }
             else
                 upperCaseCounter = 0;
-            if (i == string.length() - 1)
+            if (i == string.length() - 1 )
                 return true;
         }
         return false;
@@ -55,14 +75,60 @@ public class NamingConventions {
 
     private static boolean isValidConstant(String string){
         char character;
+        int underLineCounter = 0;
 
         for(int i = 0; i<string.length(); i++){
             character = string.charAt(i);
-            if(!Character.isUpperCase(character) && character!='_' || i == string.length()-1 && character == '_'){
+            if(!Character.isUpperCase(character) && character!='_' || (i == string.length()-1 && character == '_') || underLineCounter ==2){
                 return false;
             }
+            if(character=='_'){
+                underLineCounter++;
+            }
+            else
+                underLineCounter = 0;
         }
         return true;
     }
+
+    public static String fromConstToVariable(String string){
+        if(!isFollowingConvention(string, Convention.CONSTANT))
+            return null;
+        char character;
+        StringBuilder stringVariable = new StringBuilder();
+        int upperCase = 0;
+        for(int i = 0; i<string.length(); i++){
+            character = string.charAt(i);
+            if(Character.isLetter(character) && upperCase == 0){
+                stringVariable.append(Character.toLowerCase(character));
+            } else if (upperCase == 1) {
+                stringVariable.append(Character.toUpperCase(character));
+                upperCase = 0;
+            } else {
+                upperCase=1;
+            }
+        }
+        return stringVariable.toString();
+    }
+
+    public static String fromVariableToConst(String string){
+        if(!isFollowingConvention(string, Convention.VARIABLE))
+            return null;
+        char character;
+        StringBuilder stringConstant = new StringBuilder();
+        for(int i = 0; i<string.length(); i++){
+            character = string.charAt(i);
+            if (Character.isLowerCase(character)){
+                stringConstant.append(Character.toUpperCase(character));
+            }
+            else {
+                stringConstant.append("_");
+                stringConstant.append(character);
+            }
+        }
+        return stringConstant.toString();
+    }
+
+
 }
 
